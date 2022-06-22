f0f550359be31a8a97d8757350b8fccae1755f79
Ashley Smith <ashlynn4567@gmail.com>
2022-06-22 09:11:51 -0500

    updated server.js to instantiate application, use middleware to direct to routes, and connect to db through mongoose

---
+++ server.js
@@ -0,0 +1,27 @@
+// imports 
+const express = require('express');
+const mongoose = require("mongoose");
+
+// instantiate the app with a variable port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// implement formatting of data using middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// uncomment below if front-end is added to public folder later
+// app.use(express.static('public'));
+
+// directory
+app.use(require('./routes'));
+
+// tell mongoose to connect to db. it will create the db if it doesn't already exist
+mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost:27017/social_network", {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+// log mongo queries being executed
+mongoose.set("debug", true);
+
+// set up app to listen on a variable port
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
