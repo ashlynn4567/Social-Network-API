82ab4122e2fcbfcbaa0eb633db92f24f5e182544
Ashley Smith <ashlynn4567@gmail.com>
2022-06-22 15:04:29 -0500

    created all user crud methods in user-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -38,8 +38,7 @@ const thoughtController = {
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
-            })
-
+            });
     }, 
 
     // add thought to user
@@ -53,13 +52,7 @@ const thoughtController = {
                     { new: true }
                 );
             })
-            .then(dbThoughtData => {
-                if (!dbThoughtData) {
-                    res.status(404).json({ message: "No thought found with this id! "})
-                    return;
-                };    
-                res.json(dbThoughtData);
-            })
+            .then(dbThoughtData => res.json(dbThoughtData))
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,25 +3,151 @@ const { User } = require("../models");
 
 const userController = {
     // get all users
-    getAllUsers() {},
+    getAllUsers() {
+        User
+            .find({})
+            .populate({
+                path: "thought", 
+                select: "-__v"
+            })
+            .select("-__v")
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 
     // get ONE user by id
-    getOneUser() {},
+    getOneUser() {
+        User
+            .findOne({ _id: params.id })
+            .populate({
+                path: "thought", 
+                select: "-__v"
+            })
+            .select("-__v")
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "No user found with this id! "})
+                    return;
+                };    
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 
     // create new user
-    createUser() {},
+    createUser() {
+        User
+            .create(body)
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "No user found with this id! "})
+                    return;
+                };    
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 
     // update user by id
-    updateUser() {},
+    updateUser() {
+        User
+            .findOneAndUpdate(
+                { _id: params.id },
+                body, 
+                {
+                    new: true, 
+                    runValidators: true
+                }
+            )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "No user found with this id! "})
+                    return;
+                };    
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 
     // add friend to user
-    addFriend() {},
+    addFriend() {
+        User
+            .findOneAndUpdate(
+                { _id: params.id },
+                { $push: { friends: params.friendId } },
+                { new: true }
+            )
+            .populate(
+                {
+                    path: "friends",
+                    select: "-__v"
+                }
+            )
+            .select("-__v")
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "No user found with this id! "})
+                    return;
+                };    
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 
     // delete user by id
-    deleteUser() {},
+    deleteUser() {
+        User
+            .findOneAndDelete({ _id: params.id })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 
     // delete friend by id
-    deleteFriend() {}
+    deleteFriend() {
+        User
+            .findOneAndUpdate(
+                { _id: params.id },
+                { $pull: { friends: params.friendId }},
+                { new: true }
+            )
+            .populate(
+                { 
+                    path: "friends",
+                    select: "-__v"
+                }
+            )
+            .select("-__v")
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "No user found with this id! "})
+                    return;
+                };    
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
 };
 
 // exports
